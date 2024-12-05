a550a1f608e157a676c02aba4e2f9bcc0c0e6682
baicaiyihao <stom698@163.com>
2024-12-05 23:39:51 +0800

    update

--- project/crab/tailwind.config.js
+++ project/crab/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html", // 确保包含入口 HTML 文件
     "./src/**/*.{js,ts,jsx,tsx}", // 包括所有 React 和 TypeScript 文件
@@ -9,4 +9,4 @@ module.exports = {
     extend: {}, // 你可以在这里自定义主题
   },
   plugins: [], // 添加需要的 Tailwind 插件
-};
+};这样配置的话没地方调用也没关系吗
